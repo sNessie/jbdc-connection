0d7ca1871d527997e6c03c9691fce8172c8709b5
sNessie <a.czajczyk@icloud.com>
2019-10-13 13:55:41 +0200

    PreparedStatment and ResultSet and SOUT results

--- src/main/java/connections/Main.java
+++ src/main/java/connections/Main.java
@@ -1,10 +1,9 @@
 package connections;
 
 import javax.xml.crypto.Data;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+
+import static connections.Properties.*;
 
 public class Main {
 
@@ -12,17 +11,39 @@ public class Main {
         //Connection - java.sql
         //ResultSet
         //PrepareStatement
-
-        try {
-            Class.forName("org.postgresql.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/exchange", "postgres", "postgres" );
-            getDbInfo(connection);
-        } catch (ClassNotFoundException  | SQLException e) {
-            e.printStackTrace();
-        }
+        Connection connection = createConnection();
+        query(connection, "select * from exchanges");
     }
 
-    public static void getDbInfo(Connection connection){
+    public static Connection createConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            getDbInfo(connection);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        if (connection != null) {
+            getDbInfo(connection);
+        }
+        return connection;
+    }
+
+    public static void query(Connection connection, String sql) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                System.out.println(resultSet.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void getDbInfo(Connection connection) {
         try {
             DatabaseMetaData metaData = connection.getMetaData();
             System.out.println(
---
+++ src/main/java/connections/Properties.java
@@ -0,0 +1,8 @@
+package connections;
+
+public class Properties {
+    public static final String DB_DRIVER = "org.postgresql.Driver";
+    public static final String DB_URL = "jdbc:postgresql://localhost:5432/exchange";
+    public static final String DB_USER = "postgres";
+    public static final String DB_PASSWORD = "postgres";
+}
